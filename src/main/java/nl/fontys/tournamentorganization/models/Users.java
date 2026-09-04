2835d8f793921889ffd6244014d387839247b903
Corbex <ziggycorbey@gmail.com>
2026-09-04 15:31:07 +0200

    Added Users model class

--- src/main/java/nl/fontys/tournamentorganization/models/Users.java
+++ src/main/java/nl/fontys/tournamentorganization/models/Users.java
@@ -1,4 +1,21 @@
 package nl.fontys.tournamentorganization.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tournament")
 public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
 }
