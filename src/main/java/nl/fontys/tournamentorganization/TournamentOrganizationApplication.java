958fa0b8ade123702e6a7349bbeb8b9c8987acf0
Corbex <ziggycorbey@gmail.com>
2026-09-04 09:59:09 +0200

    initial commit

---
+++ src/main/java/nl/fontys/tournamentorganization/TournamentOrganizationApplication.java
@@ -0,0 +1,13 @@
+package nl.fontys.tournamentorganization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentOrganizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TournamentOrganizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/fontys/tournamentorganization/TournamentOrganizationApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.fontys.tournamentorganization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentOrganizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
