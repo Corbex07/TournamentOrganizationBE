ab84704aee8cf9164f231807e474556fbaeedf08
Corbex <ziggycorbey@gmail.com>
2026-09-04 19:03:10 +0200

    added connection to database

--- src/main/java/nl/fontys/tournamentorganization/models/Tournament.java
+++ src/main/java/nl/fontys/tournamentorganization/models/Tournament.java
@@ -8,11 +8,14 @@ public class Tournament {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "name")
     private String name;
+
     @ManyToOne
     @JoinColumn(name = "organiser_id")
     private Users organiser;
+
     @Column(name = "max_capacity")
     private Integer maxCapacity;
 }
