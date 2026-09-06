3947b422965304b598c7bc1ff83143de6da6f9d2
Corbex <ziggycorbey@gmail.com>
2026-09-06 17:11:00 +0200

    Added tournament controller
    - added api for getall, getone, save and delete tournament

--- src/main/java/nl/fontys/tournamentorganization/controllers/TournamentController.java
+++ src/main/java/nl/fontys/tournamentorganization/controllers/TournamentController.java
@@ -1,4 +1,46 @@
 package nl.fontys.tournamentorganization.controllers;
 
+import nl.fontys.tournamentorganization.Repositories.TournamentRepository;
+import nl.fontys.tournamentorganization.models.Tournament;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tournaments")
 public class TournamentController {
+
+    private final TournamentRepository tournamentRepository;
+
+    public TournamentController(TournamentRepository tournamentRepository) {
+        this.tournamentRepository = tournamentRepository;
+    }
+    @GetMapping("/tournaments")
+    List<Tournament> all() {
+        return tournamentRepository.getAllTournaments();
+    }
+
+    @PostMapping("/tournaments/{id}")
+    Tournament newTournament(@RequestBody Tournament newTournament) {
+        return tournamentRepository.saveTournament(newTournament);
+    }
+
+    @GetMapping("/tournaments/{id}")
+    Tournament one(@PathVariable Long id) {
+
+        return tournamentRepository.getTournamentById(id)
+                .orElseThrow(() ->
+                        new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Tournament not found"
+                    )
+                );
+    }
+
+    @DeleteMapping("/employees/{id}")
+    void deleteEmployee(@PathVariable Long id) {
+        tournamentRepository.deleteTournament(id);
+    }
 }
